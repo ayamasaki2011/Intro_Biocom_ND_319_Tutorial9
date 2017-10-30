15a989a82a0cac343ce14dca866b348fe5d2827d
Joe C <jchambe5@nd.edu>
2017-10-30 11:55:44 -0400

    updated some comments and added the code to obtain p values for mutations 2 and 3 though still not getting correct p values for any of question 1

--- 9Answers.py
+++ 9Answers.py
@@ -1,21 +1,26 @@
+#Exercise 09
+# Question 1 zebrafish mutations
+
+#import pandas and read csv file
 import pandas
 file=pandas.read_csv("ponzr1.csv",header=0,sep=",")
 
+#subset each mutation values with WT values
 mut1=file.loc[file.mutation.isin(['WT', 'M124K']),:]
 mut2=file.loc[file.mutation.isin(['WT', 'V456D']),:]
 mut3=file.loc[file.mutation.isin(['WT', 'I213N']),:]
 
-#mutation 1 subset into a new dataframe
+#mutation 1 subset into a new dataframe and changes the x column to 0's and 1's
 mut12=pandas.DataFrame({'y':mut1.ponzr1Counts, 'x':0})
 mut12.loc[mut1.mutation=='M124K', 'x']=1
 
-#mutation 2 subset into a new dataframe
+#mutation 2 subset into a new dataframe and changes the x column to 0's and 1's
 mut22=pandas.DataFrame({'y':mut2.ponzr1Counts, 'x':0})
-mut22.loc[mut1.mutation=='V456D', 'x']=1
+mut22.loc[mut2.mutation=='V456D', 'x']=1
 
-#mutation 3 subset into a new dataframe
+#mutation 3 subset into a new dataframe and changes the x column to 0's and 1's
 mut32=pandas.DataFrame({'y':mut3.ponzr1Counts, 'x':0})
-mut32.loc[mut1.mutation=='I213N', 'x']=1
+mut32.loc[mut3.mutation=='I213N', 'x']=1
 
 #import packages
 import numpy
@@ -24,6 +29,7 @@ from scipy.optimize import minimize
 from scipy.stats import norm
 from plotnine import *
 
+#plot the values for WT at 0 and mutation 1 at 1 --- not sure if we need to do this
 ggplot(mut12,aes(x='x',y='y'))+geom_point()+theme_classic()
 
 #null hypothesis likelihood equation
@@ -55,7 +61,37 @@ print(fitAlt.x)
 from scipy.stats import chi2
 D=(2*(fitAlt.fun-fitNull.fun))
 mut12answer=1-chi2.cdf(x=D,df=1)
+print('mutation M124K p value')
 print(mut12answer)
+
+
+## for mutation 2
+fitNull=minimize(fun1a,initialVals1, method="Nelder-Mead",options={'disp': True}, args=mut22)
+fitAlt=minimize(fun1b,initialVals1, method="Nelder-Mead",options={'disp': True}, args=mut22)
+
+print(fitNull.x)
+print(fitAlt.x)
+
+from scipy.stats import chi2
+D=(2*(fitAlt.fun-fitNull.fun))
+mut22answer=1-chi2.cdf(x=D,df=1)
+print('mutation V456D p value')
+print(mut22answer)
+
+##for mutation 3
+
+fitNull=minimize(fun1a,initialVals1, method="Nelder-Mead",options={'disp': True}, args=mut32)
+fitAlt=minimize(fun1b,initialVals1, method="Nelder-Mead",options={'disp': True}, args=mut32)
+
+print(fitNull.x)
+print(fitAlt.x)
+
+from scipy.stats import chi2
+D=(2*(fitAlt.fun-fitNull.fun))
+mut32answer=1-chi2.cdf(x=D,df=1)
+print('mutation I213N p value')
+print(mut32answer)
+
 
 
 ## currently this does not produce the correct p value according to the p value sent to us from Stuart.
